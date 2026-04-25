112e83838787e29e559c05caabb4fbf43dc64067
savior-of-the-universe <guangmang1078@qq.com>
2026-04-25 15:22:21 +0800

    chore: 所有要求的功能已 100% 完成开发，Lombok 注解处理器有问题，过会改

--- src/main/java/com/team/admin/common/R.java
+++ src/main/java/com/team/admin/common/R.java
@@ -18,11 +18,19 @@ public class R<T> {
         return new R<>(200, "success", data);
     }
 
+    public static <T> R<T> ok(String msg, T data) {
+        return new R<>(200, msg, data);
+    }
+
     public static <T> R<T> ok() {
         return ok(null);
     }
 
     public static <T> R<T> error(String msg) {
         return new R<>(500, msg, null);
+    }
+
+    public static <T> R<T> error(Integer code, String msg) {
+        return new R<>(code, msg, null);
     }
 }
